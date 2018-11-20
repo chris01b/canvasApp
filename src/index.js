064df829ef946470929ce82618e9d3bf0858e202
chris01b <nova6ers@gmail.com>
2018-11-20 16:37:39 -0500

    Added command line quesiton list functionality

--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ const myCse = new cse("AIzaSyB_YyRZ3ePQNEephDs20BwuH_N-k1C_GH4", "00063352676834
 const myQuizlet = new Quizlet("R3snf5zu9W");
 
 async function findAnswers() {
-  let questions = ["Researchers are interested in studying the impact of drugs on human fetuses. In this case, why would a correlational study be more appropriate than an experiment?",
-  "Researchers use experiments rather than other research methods in order to distinguish between",
-  "Which of the following types of cells are located in the brain's occipital lobe?"];
-  
   let answers = [];
   await questions.forEachAsync(async (question) => {
     sets = await myCse.makeQuery(question);
@@ -23,9 +19,8 @@ async function findAnswers() {
 
 findAnswers().catch(console.error);
 
-/*
 if (module === require.main) {
-  var query = process.argv[2];
+  const fs = require('fs');
+  var questions = fs.readFileSync('../questions.txt').toString().split("\n");
   findAnswers().catch(console.error);
 }
-*/
