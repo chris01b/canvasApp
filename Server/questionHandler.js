9374ec1011b64d1908e4f8390d261fd83c08e09a
chris01b <nova6ers@gmail.com>
2019-02-25 00:26:20 -0500

    Added more error handling

--- Server/questionHandler.js
+++ Server/questionHandler.js
@@ -15,20 +15,29 @@ module.exports = () => {
   console.log('Connected to', socket.id)
 
     async function findAnswers(questions) {
-      let answers = [];
       forEachAsync(questions, async (question, index) => {
         let myCse = new cse("AIzaSyB_YyRZ3ePQNEephDs20BwuH_N-k1C_GH4", "000633526768340694300:gdwf-ty5mtk");
         let myQuizlet = new Quizlet("R3snf5zu9W");
 
         try {
           sets = await myCse.makeQuery(question);
-          answer = await myQuizlet.search(sets, question);
-          answers.push(answer);
-
-          socket.emit('returnAnswer', [index+1, answer]);
+          answers = await myQuizlet.search(sets, question);
+          if (answers.length == 0) {
+            throw new Error('Answer not in Quizlet set');
+          } else {
+            socket.emit('returnAnswer', [index+1, answers]);
+          }
         } catch(e) {
-          if (e.message == 'Cannot read property \'toString\' of undefined') {
-            console.error('No quizlets found for question', (index+1));
+          switch(e.message) {
+            case 'Cannot read property \'toString\' of undefined':
+              console.error('No quizlets found for question', (index+1));
+              break;
+            case 'Answer not in Quizlet set':
+              console.error('Could not find answer in set for question', (index+1));
+              break;
+            default:
+              console.error(e);
+              break;
           }
         }
         console.log('Submitted Answer', (index+1), 'to', socket.id);
