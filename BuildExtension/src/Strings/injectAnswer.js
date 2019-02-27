6949f549cebbc5ba7b7147a9f5779db2ae3b7cc7
chris01b <nova6ers@gmail.com>
2019-02-26 22:41:31 -0500

    Fixed incorrect question bug

--- BuildExtension/src/Strings/injectAnswer.js
+++ BuildExtension/src/Strings/injectAnswer.js
@@ -3,10 +3,20 @@ export default (number, answerHTML) => {
 
 var answerHTML = ` + '`' + answerHTML + '`' + `;
 
-$('span:contains("Question ` + number + String.raw`")[role="heading"]')
+var question = $('span:contains("Question ` + number + String.raw`")[role="heading"]')
   .filter(function() {
     return $(this).text() === ("Question ` + number + String.raw`");
-  }).parents().eq(2).after(answerHTML);
+  });
+
+var incorrectQuestion = $('span:contains("Question ` + number + String.raw`")[role="heading"]');
+var incorrectTest = incorrectQuestion.is('span[aria-describedby="user_answer_NaN_arrow"]');
+
+if (question.length == 0 && incorrectTest == true) {
+  console.log('INCORRECT');
+  question = incorrectQuestion;
+}
+
+question.parents().eq(2).after(answerHTML);
 
 console.log('Injected answer ` + number + `');
 
